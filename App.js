5831b947103fbd5182cd8a19437d470c49e0fde4
coderfauzan <mahzaibfauzan@gmail.com>
2024-01-01 20:36:27 +0530

    3rd episode

--- App.js
+++ App.js
@@ -1,23 +1,34 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [ //Create an array of children
-    React.createElement("h1", {}, "I'm a h1 tag"),
-    React.createElement("h2", {}, "I'm a h2 tag"),
-  ])
-); //Nested div creation
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading1" },
-  "Hello from react"
+//React element
+
+const heading = (
+  <h1 id="heading" className="head">
+    HI from jsx element
+  </h1>
+);
+const Title = () => (
+  <h1 id="heading" className="head">
+    HI from component
+  </h1>
 );
 
-console.log(heading); //object
-console.log(parent); //object
+//component composition
+//React component
+const HeadingComponent = () => (
+  <div id="container">
+    {heading}
+    <h1>function component</h1>
+    <Title />
+    {Title()}
+    <Title></Title>
+  </div>
+);
+
+console.log(heading);
+console.log(HeadingComponent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-root.render(parent);
+// root.render(heading);
+root.render(<HeadingComponent />);
